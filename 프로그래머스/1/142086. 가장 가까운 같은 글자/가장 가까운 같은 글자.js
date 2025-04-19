b045d06d563f0e294629ff45de70d1a57e824f41
Gahyeon <113450966+maylh@users.noreply.github.com>
2025-04-19 23:05:17 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.90 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    const answer = [];
+    const map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            answer.push(i - map.get(s[i]));
+        } else {
+            answer.push(-1);
+        }
+        map.set(s[i], i);
+    }
+
+    return answer;
+}
